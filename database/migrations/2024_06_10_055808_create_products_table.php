132cf51739f47f8e310321819feaf3728c5f5e5a
tuonglvph24343 <tuonglvph24343@fpt.edu.vn>
2024-06-14 21:08:10 +0700

    fix code database migration

--- database/migrations/2024_06_10_055808_create_products_table.php
+++ database/migrations/2024_06_10_055808_create_products_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
-            $table->integer('amount');
-            $table->integer('price');
-            $table->string('thumbail', 255);
-            $table->foreignId('brand_id')->constrained('brands');
-            $table->foreignId('category_id')->constrained('categories');
+            $table->unsignedBigInteger('brand_id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->decimal('price', 10, 2);
+            $table->text('description')->nullable();
+            $table->string('status')->default('active');
+            $table->foreign('brand_id')->references('id')->on('brands');
+            $table->foreign('category_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
