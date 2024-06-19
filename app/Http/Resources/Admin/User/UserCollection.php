0c8960e8c26eabdb824c2297553efa1a3b863b8a
DuongLQ <duonglq@regit-technology.com>
2024-06-19 22:35:11 +0700

    base API admin + list user

---
+++ app/Http/Resources/Admin/User/UserCollection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Admin\User;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class UserCollection extends ResourceCollection
+{
+    public function toArray($request)
+    {
+        $paginator = $this->resource;
+        $baseData = ['data' => UserResource::collection($paginator)];
+
+        if (data_get($request, 'all')) return $baseData;
+        return [
+            ...$baseData,
+            'per_page' => $paginator->perPage(),
+            'total_page' => $paginator->lastPage(),
+            'current_page' => $paginator->currentPage(),
+            'total' => $paginator->total(),
+        ];
+    }
+}
